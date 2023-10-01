bcf689a2558cf7a236340a738dd4365bad60c7da
farahzet <119497241+farahzet@users.noreply.github.com>
2023-10-01 10:12:50 +0700

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header"
 import Form from "./components/Form"
 import React, {components} from 'react'
-import bootstrap_logo from "./assets/bootstrap_logo.png"
 import Button from "./components/Button"
 import './Home.css'
 import Article from "./components/Article"
